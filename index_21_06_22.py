161d69555a7ba138e5193a4515a76571315e1967
ArthurIpolitov <86003479+ArthurIpolitov@users.noreply.github.com>
2021-06-22 16:59:41 +0300

    Second project 06 18 2021

---
+++ index_21_06_22.py
@@ -0,0 +1,78 @@
+"""
+STRING LESSON
+"""
+
+# someString = ' '
+# print(type(someString))
+
+# stringSample = 'Hello World world'
+# stringSample2 = 'first letteR is lowErcase'
+# stringSample3 = '            extra whitespace string                                             '
+
+""" print(variable[Start:Stop:step]) """
+# print(stringSample[0:5:2])
+# print(stringSample[0:])
+# print(stringSample[2:])
+# print(stringSample[:2])
+
+# print(len(stringSample))
+# lenString = len(stringSample2)
+# print(stringSample2[0:lenString])
+
+# print('world' in stringSample)
+""" LOWER/UPPER EXAMPLES """
+# print('Hello World'.lower())
+# print(stringSample.lower())
+""" CAPITALIZE EXAMPLES """
+# print(stringSample.capitalize())
+""" STRIP EXAMPLE """
+# print(stringSample3.strip().strip('e'))
+""" REPLACE EXAMPLES """
+# print(stringSample.replace('world','duck'))
+""" SLICE EXAMPLES """
+# stringSlice = stringSample[0:5]
+# print(stringSlice)
+""" COUNT EXAMPLES """
+# countNumber = stringSample.count('World')
+# print(countNumber)
+""" FIND EXAMPLES """
+# foundIndex = stringSample.find('World')
+# print(stringSample[foundIndex:])
+
+""" TEXT FORMATTING """
+# somethingOne = input('Enter your Something: ')
+# somethingTwo = input('Enter your Second Something ')
+# new_string = urSalary + ' ' + str(salary)
+# formatedString = 'Your salary is {1}, or {0}'
+# print(formatedString.format(salary, newSalary))
+
+# formattedString = 'This is {somethingOnez:}. It costs {price:.2f} eur'
+# print(formattedString.format(product=somethingOne, price=somethingTwo))
+
+# emp_string = f'Hello my name is {somethingOne}. My salary is {somethingTwo}'
+# print(emp_string)
+
+""" SLASH EXAMPLES """
+
+# someString = 'Roman that\'s \nname'
+# print(someString)
+
+""" IF ELSE ELIF EXAMPLES """
+
+# redCol = "\033[31m"
+# greenCol = "\033[32m"
+# endCol = '\033[0m'
+#
+# id_code = input('Input your ID CODE: ')
+# if len(id_code) == 11:
+#     if id_code[0] == '3' or id_code[0] == '4':
+#         print(greenCol + 'This is true ID CODE' + endCol)
+#     else:
+#         print(redCol + 'This is false ID CODE' + endCol)
+# else:
+#     print(redCol + 'This is false ID CODE. ID CODE lenght is ' + str(len(id_code))+ endCol)
+
+""" TASK FOR HOME """
+""" Введите цифры чтобы получить другие варианты """
+""" END TASK TEXT """
+
