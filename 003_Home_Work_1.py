3b9d87063a500338f3d855c54811fe2703e560cb
ArthurIpolitov <86003479+ArthurIpolitov@users.noreply.github.com>
2021-06-27 22:37:49 +0300

    New HomeWork

---
+++ 003_Home_Work_1.py
@@ -0,0 +1,22 @@
+'''
+Для диапозона чисел 0-100
+
+Если число делится на 3 без остатка - написать Fizz
+
+Если число делится на 5 без остатка - написать Buzz
+
+Если число делится на 3 и на 5 без остатка - написать FizzBuzz
+'''
+print("---------------------------------------")
+user_number = input("Введите число: ")
+
+for new_num in range(1, int(user_number)):
+    if new_num % 3 == 0:
+        # print( str(new_num) + " число делится без остатка на 3| Введенное число: " + user_number)
+        print("Fizz")
+    if new_num % 5 == 0:
+        # print( str(new_num) + " число делится без остатка на 5| Введенное число: " + user_number)
+        print("Buzz")
+    if new_num % 3 == 0 and new_num % 5 == 0:
+        # print( str(new_num) + " число делится без остатка на 3 и на 5| Введенное число: " + user_number)
+        print("FizzBuzz")
