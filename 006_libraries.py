046ca28bec8721c69d600646300f5139392e2de8
ArthurIpolitov <86003479+ArthurIpolitov@users.noreply.github.com>
2021-07-06 16:49:04 +0300

    Homework nd isikukood validation

---
+++ 006_libraries.py
@@ -0,0 +1,20 @@
+'''
+МОДУЛИ И БИБЛИОТЕКИ
+'''
+
+# import math
+import calendar
+#
+# print(math.pi)
+#
+# print(math.sqrt(25))
+#
+# print(math.ceil(25.523))
+#
+# print(math.floor(25.523))
+#
+# print(math.radians(90))
+# print(math.degrees(90))
+# print(math.sin(90))
+for num in range(1,13):
+    print(calendar.month(2000, num, w=2, l=1))
