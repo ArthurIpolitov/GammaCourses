9327f8a6bb41e1a49f6326210bd772271b571e8b
ArthurIpolitov <86003479+ArthurIpolitov@users.noreply.github.com>
2021-07-09 16:49:16 +0300

    write add and some other

--- 007_write_read_files/007_write_read.py
+++ 007_write_read_files/007_write_read.py
@@ -30,8 +30,24 @@ FILE SYSTEMS
     #     file_content = file.read(100)
     #     print(file_content)
     # file.write(some_text)
-with open('text_files//test_file.txt', 'r', encoding='utf8') as file2:
-    print(file2.read(10))
-    print(file2.seek(0))
-    print(file2.read(10))
+# with open('text_files//test_file.txt', 'r', encoding='utf8') as file2:
+#     print(file2.read(10))
+#     print(file2.seek(0))
+#     print(file2.read(10))
+
+# with open('text_files//threemusketeer.txt', 'r', encoding='utf-8') as rfile:
+#     with open('text_files//threemusketeerCopy.txt', 'w', encoding='utf8') as wfile:
+#         file_content = rfile.read(100)
+#         while len(file_content) == 100:
+#             wfile.write(file_content)
+#             file_content = rfile.read(100)
+
+# with open('text_files//python.jpg', 'rb') as rfile:
+#     with open('text_files//python_copy.jpg', 'wb') as wfile:
+#         file_content = rfile.read(20)
+#         while len(file_content) > 0:
+#             wfile.write(file_content)
+#             file_content = rfile.read(20)
+
+
 
